6a8af9782bb2037d7d21c222ab2b83e802d19876
NacimNB <nacim.naitbaha@outlook.com>
2022-06-06 19:56:58 +0200

    progression page

---
+++ progression/progession.js
@@ -0,0 +1,52 @@
+function create_tr(table_id) {
+    let table_body = document.getElementById(table_id),
+        first_tr   = table_body.firstElementChild
+        tr_clone   = first_tr.cloneNode(true);
+
+    table_body.append(tr_clone);
+
+    clean_first_tr(table_body.firstElementChild);
+}
+
+function clean_first_tr(firstTr) {
+    let children = firstTr.children;
+    
+    children = Array.isArray(children) ? children : Object.values(children);
+    children.forEach(x=>{
+        if(x !== firstTr.lastElementChild)
+        {
+            x.firstElementChild.value = '';
+        }
+    });
+}
+
+
+
+function remove_tr(This) {
+    if(This.closest('tbody').childElementCount == 1)
+    {
+        alert("You Don't have Permission to Delete This ?");
+    }else{
+        This.closest('tr').remove();
+    }
+}
+
+
+
+
+const editables = document.querySelectorAll("[contenteditable]");
+
+// save edits
+editables.forEach(el => {
+  el.addEventListener("blur", () => {
+    localStorage.setItem("dataStorage-" + el.id, el.innerHTML);
+  })
+});
+
+// once on load
+for (var key in localStorage) {
+  if (key.includes("dataStorage-")) {
+    const id = key.replace("dataStorage-","");
+    document.querySelector("#" + id).innerHTML = localStorage.getItem(key);
+  }
+}
