04d9cb8235bed2ff0a6530dd4638ce68d9f8369e
NacimNB <nacim.naitbaha@outlook.com>
2022-06-06 20:03:50 +0200

    &

--- progression/progession.js
+++ progression/progession.js
@@ -33,20 +33,3 @@ function remove_tr(This) {
 
 
 
-
-const editables = document.querySelectorAll("[contenteditable]");
-
-// save edits
-editables.forEach(el => {
-  el.addEventListener("blur", () => {
-    localStorage.setItem("dataStorage-" + el.id, el.innerHTML);
-  })
-});
-
-// once on load
-for (var key in localStorage) {
-  if (key.includes("dataStorage-")) {
-    const id = key.replace("dataStorage-","");
-    document.querySelector("#" + id).innerHTML = localStorage.getItem(key);
-  }
-}
